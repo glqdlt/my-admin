faab955b9376ee293a44156f567bfb5e6004519c
glqdlt <dlfdnd0725@gmail.com>
2018-02-06 15:16:34 +0900

    First

---
+++ admin-web-platform/src/main/java/com/glqdlt/adminwebplatform/AdminWebPlatformApplication.java
@@ -0,0 +1,12 @@
+package com.glqdlt.adminwebplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminWebPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminWebPlatformApplication.class, args);
+	}
+}
