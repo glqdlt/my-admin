f3ab7e271c8dd5090fe190e7b5f2b0308ff93de1
glqdlt <dlfdnd0725@gmail.com>
2018-02-06 15:26:06 +0900

    Set Project Struct

--- admin-web-platform/src/main/java/com/glqdlt/adminwebplatform/AdminWebPlatformApplication.java
+++ admin-web-platform/src/main/java/com/glqdlt/adminwebplatform/AdminWebPlatformApplication.java
@@ -2,11 +2,19 @@ package com.glqdlt.adminwebplatform;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
 @SpringBootApplication
 public class AdminWebPlatformApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AdminWebPlatformApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AdminWebPlatformApplication.class, args);
+    }
+
+    @RequestMapping("/")
+    String root() {
+        return "index";
+    }
 }
